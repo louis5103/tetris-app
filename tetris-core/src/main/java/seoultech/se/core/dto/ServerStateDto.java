ddc67bfc2a16c32709c2d3c005e56823a15925d3
louis5103 <louis38625103@gmail.com>
2025-11-26 01:34:19 +0900

    [Feat] 양단간 Dto 생성

---
+++ tetris-core/src/main/java/seoultech/se/core/dto/ServerStateDto.java
@@ -0,0 +1,21 @@
+package seoultech.se.core.dto;
+
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import seoultech.se.core.GameState;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServerStateDto {
+    private long lastProcessedSequence; // 서버가 처리한 마지막 시퀀스 번호
+    private GameState myGameState;      // 서버가 확정한 나의 상태 (보정용)
+    private GameState opponentGameState;// 상대방 상태 (렌더링용)
+    private List<String> events;        // 발생한 이벤트 (LINE_CLEAR, ATTACK 등)
+}
